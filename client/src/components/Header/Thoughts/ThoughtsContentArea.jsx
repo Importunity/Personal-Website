b8bcb7d4545339abf9d20da5d7a7e3e77dfa1573
Amadeus Her <Amadeusherr@gmail.com>
2020-10-18 03:24:32 -0500

    update thoughts ui to give user instructions

--- client/src/components/Header/Thoughts/ThoughtsContentArea.jsx
+++ client/src/components/Header/Thoughts/ThoughtsContentArea.jsx
@@ -4,14 +4,14 @@ import Note from './Note';
 import {connect} from 'react-redux';
 import {getNotes, deleteNote, addNote} from '../../../actions/noteActions';
 import PropTypes from 'prop-types';
-import { Pagination } from '@material-ui/lab';
 import { TablePagination } from '@material-ui/core';
 import '../../../styles/ThoughtsContentArea.css';
+import MainThoughtsContent from './MainThoughtsContent';
 
 
 function ThoughtsContentArea(props){
     const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [rowsPerPage, setRowsPerPage] = useState(4);
     const handleChangePage = (event, newPage) => {
       setPage(newPage);
     };
@@ -43,9 +43,14 @@ function ThoughtsContentArea(props){
         setSearch(event.target.value.substr(0,20));
     }
     const filteredNotes = notes.filter((note) => note.title.toLowerCase().indexOf(search.toLowerCase()) !== -1);
+    const[clickedNote, setClickedNote] = useState({title: '', content: '', clicked: false});
+    function handleClickedNote(note) {
+        setClickedNote({title: note.title, content: note.content, clicked: true});
+    }
+
     return (
-        <div className="">
-            <div className="row">
+        <div className="thoughts-content">
+            <div className="row ">
                 <div className="col-md-3">
                     <div className="search-container">
                         <form className="search-form">
@@ -59,13 +64,19 @@ function ThoughtsContentArea(props){
                     .map((objectNote, index) => {
                         //console.log(index);
                         return (
-                            <Note key={objectNote._id} id={objectNote._id} title={objectNote.title} content={objectNote.content} onDelete={deleteNote} />
+                            <div key={objectNote._id} onClick={() => handleClickedNote(objectNote)}>
+                                <Note isAuthenticated={isAuthenticated} id={objectNote._id} title={objectNote.title} content={objectNote.content} onDelete={deleteNote} />
+                            </div>
                         );
                     })}
                     <TablePagination component="div" className="pagination" rowsPerPageOptions={[]} count={notes.length} rowsPerPage={rowsPerPage} page={page} onChangePage={handleChangePage} onChangeRowsPerPage={handleChangeRowsPerPage} width="90%" />
                 </div>
                 <div className="col-md-9 main-content">
-
+                    {clickedNote.clicked? (
+                        <MainThoughtsContent note={clickedNote}></MainThoughtsContent>
+                    ) : (
+                        <div className="empty-content">Click a tab from the left side to read more.</div>
+                    )}
                 </div>
             </div>
         </div>

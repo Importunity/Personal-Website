1eb32b3cd33abb3214bd670f751c65afe2e8f502
Amadeus Her <Amadeusherr@gmail.com>
2020-10-18 03:18:39 -0500

    Add simple message

--- client/src/components/Header/About.jsx
+++ client/src/components/Header/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../../styles/About.css';
 
 function About() {
-  return <div className="about-container">hello</div>;
+  return <div className="about-container">Intentionally left blank. Not sure why someone would want to know about me.</div>;
 }
 
 export default About;

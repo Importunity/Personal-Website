6b6c34147c968fd3a653dc9f77293da635c0f416
Amadeus Her <Amadeusherr@gmail.com>
2020-11-11 13:15:59 -0600

    Remove stop

--- client/src/components/Projects/Algorithms/Sorting/BubbleSort.js
+++ client/src/components/Projects/Algorithms/Sorting/BubbleSort.js
@@ -12,15 +12,12 @@ const swap = (arr, firstIndex, secondIndex) => {
 
 function BubbleSort(props){
     const[values, setValues] = useState(props.values);
-    const[stop, setStop] = useState(false);
     useEffect(() => {
         setValues(props.values);
     }, [props.values])
-    console.log(stop);
     async function bubbleSort(){
         var tempArr = values;
         var sorted = false;
-        setStop(false);
         while(!sorted){
             sorted = true;
             for(var i = 0; i < tempArr.length; i++){
@@ -39,7 +36,6 @@ function BubbleSort(props){
             <div className="container mt-5 " >
                 <Button className="sort-button" variant="contained" color="inherit" onClick={bubbleSort}>START</Button>
                 <Button variant="contained" color="inherit" className="sort-button" onClick={() => props.handleCounterChange(props.counter + 1)}>Generate New Values</Button>
-                <Button variant="contained" color="inherit" className="sort-button" onClick={() => setStop(true)}>Stop</Button>
 
             </div>
             <div className="algo-container">

f25425e22920b694168727b3a5b6f2e4cde39d56
Amadeus Her <Amadeusherr@gmail.com>
2020-12-21 16:07:48 -0600

    remove unused code

--- client/src/index.js
+++ client/src/index.js
@@ -10,34 +10,11 @@ import 'jquery/dist/jquery';
 import 'antd/dist/antd.css';
 
 
-var isMobile = false;
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-  // true for mobile device
-  isMobile = true;
-}else{
-  // false for not mobile device
-  isMobile = false;
-}
-//console.log(isMobile);
 
 ReactDOM.render(
-
     <App />,
   document.getElementById("root")
 );
-/*if(!isMobile){
-    ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  );
-}else{
-  ReactDOM.render(
-    <div>
-      <h1>Hello, I am currently in the process of optimizing my personal website for mobile, so please use a computer. Thank you.</h1>
-    </div>,
-  document.getElementById('root')
-  );
-}*/
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
